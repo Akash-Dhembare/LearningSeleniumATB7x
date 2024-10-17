66158936b1ae976d9ff927a9f60c60b05ab542b0
Akash <akash.dhembare777@gmail.com>
2024-10-17 15:46:46 +0530

    Initial commit

---
+++ src/test/java/com/akash/dhembare2000/ex_Selenium/Selenium02.java
@@ -0,0 +1,14 @@
+package com.akash.dhembare2000.ex_Selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Selenium01 {
+    public static void main(String[] args) {
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://app.vwo.com");
+
+
+    }
+}
---
+++ src/test/java/com/akash/dhembare2000/ex_Selenium/Selenium10.java
@@ -0,0 +1,2 @@
+package com.akash.dhembare2000.ex_Selenium;public class Selenium10 {
+}
